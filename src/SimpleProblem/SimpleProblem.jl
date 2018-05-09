38b1f18e698a3452a00072deb74f4906353da222
mbesancon <mathieu.besancon@gmail.com>
2018-05-08 20:00:53 -0400

    core simple problem

--- src/SimpleProblem/SimpleProblem.jl
+++ src/SimpleProblem/SimpleProblem.jl
@@ -3,5 +3,39 @@
 """
 module SimpleProblem
 
+import MathProgBase
+
+mutable struct SimpleSubProblem <: AbstractSubProblem
+    costs::AbstractVector{N1}
+    A::AbstractMatrix{N2}
+    sense::AbstractVector{Char}
+    b::AbstractVector{N3}
+    vartypes::AbstractVector{Symbol}
+    lb::AbstractVector{N4}
+    ub::AbstractVector{N5}
+    solver::AbstractMathProgSolver
+end
+
+mutable struct SimpleMasterProblem{SimpleSubProblem,N} <: AbstractMasterProblem{SimpleSubProblem} where {N1<:Number,N2<:Number,N3<:Number,N4<:Number,N5<:Number}
+    costs::AbstractVector{N1}
+    A::AbstractMatrix{N2}
+    sense::AbstractVector{Char}
+    b::AbstractVector{N3}
+    vartypes::AbstractVector{Symbol}
+    lb::AbstractVector{N4}
+    ub::AbstractVector{N5}
+    solver::AbstractMathProgSolver
+end
+
+# TODO build convenient constructor functions for SimpleMasterProblem
+
+"""
+    solve implementation for SimpleSubProblem
+"""
+function solve(sp::SimpleSubProblem,π::V1,σ::V2) where {V1<:AbstractVector{N1}, V2<:AbstractVector{N2}} where {N1<:Real, N2<:Real}
+    reduced_costs = [t[1] - t[2] for t in zip(sp.costs,π)] - σ
+    result = MathProgBase.mixintprog(reduced_costs, sp.A, sp.sense, sp.b, sp.vartypes, sp.lb, sp.ub, sp.solver)
+    return (result.status, [result.objval], [result.sol])
+end
 
 end
