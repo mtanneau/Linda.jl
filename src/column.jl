b09a01e6db958011bb112d704405cd3b4e6252e4
mtanneau <mathieu.tanneau@gmail.com>
2018-07-24 15:18:19 -0400

    Parametrized Column type

--- src/column.jl
+++ src/column.jl
@@ -3,11 +3,36 @@
 
     Data structure for holding columns and related information.
 """
-mutable struct Column 
-    cost::Float64       # Native cost of the column
-    col::AbstractVector{Float64} # column coefficients
+mutable struct Column{Tv<:Real, Tc<:AbstractVector{Tv}}
+    cost::Float64        # Native cost of the column
+    col::Tc              # column coefficients
     is_vertex::Bool      # Whether column is vertex (true) or ray (false)
     is_in_rmp::Bool      # Whether column is currently in the RMP
     idx_subproblem::Int  # Index of corresponding sub-problem
     idx_column::Int      # Index of the column in the RMP
+
+    Column(
+        cost,
+        col::Tc,
+        is_vertex,
+        idx_subproblem
+    ) where{Tv<:Real, Tc<:AbstractArray{Tv}} = new{Tv, Tc}(cost, col, is_vertex, false, idx_subproblem, -1)
 end
+
+"""
+    get_reduced_cost()
+
+Compute reduced cost of column given current dual iterate
+"""
+get_reduced_cost(
+    c::Column{Tv, Tc},
+    π::AbstractVector{Float64},
+    σ::Real
+) where{Tv<:Real, Tc<:AbstractArray{Tv}} = c.cost - dot(π, c.col) - σ
+
+get_reduced_cost(
+    c::Column{Tv, Tc},
+    π::AbstractVector{Float64},
+    σ::AbstractVector{Float64}
+) where{Tv<:Real, Tc<:AbstractArray{Tv}} = 
+    get_reduced_cost(c, π, σ[c.idx_subproblem])
--- test/master.jl
+++ test/master.jl
@@ -1,54 +1,80 @@
+function create_mp(m, R, b)
+
+    rmp = MPB.LinearQuadraticModel(ClpSolver())
+    # Convexity constraints
+    for r in 1:R
+        MPB.addconstr!(rmp, Vector{Float64}(), Vector{Float64}(), 1.0, 1.0)
+    end
+
+    # linking constraints
+    for i in 1:m
+        MPB.addconstr!(rmp, Vector{Float64}(), Vector{Float64}(), b[i], b[i])
+    end
+
+    # Artificial variables
+    for i in 1:m
+        MPB.addvar!(rmp, [R+i], [1.0], 0.0, Inf, 10^4)  # slack
+        MPB.addvar!(rmp, [R+i], [-1.0], 0.0, Inf, 10^4)  # surplus
+    end
+
+    # Instanciate RMP
+    mp = Linda.LindaMaster(rmp, R, m, b)
+
+    return mp
+end
+
+function add_initial_columns!(mp, m, R)
+
+    # Add an initial set of columns
+    Linda.add_columns!(
+        mp,
+        [Linda.Column(0.0, zeros(m), true, r) for r in 1:R]
+    )
+
+    # Add some other columns
+    for r in 1:R
+        col = Linda.Column(1.0, rand(m), true, r)
+        Linda.add_column!(mp, col)
+        # Add column again, this should not do anything
+        Linda.add_columns!(mp, [col])
+    end
+
+    return nothing
+end
+
 # Create initial RMP
-m = 1  # number of linking constraints
-R = 1  # number of sub-problems
+srand(0)
+m = 2  # number of linking constraints
+R = 2  # number of sub-problems
+b = zeros(m)  # Right-hand side of linking constraints
 
-b = ones(m)
+mp = create_mp(m, R, b)
+add_initial_columns!(mp, m, R)
 
-rmp = MPB.LinearQuadraticModel(ClpSolver())
-# Convexity constraints
-for r in 1:R
-    MPB.addconstr!(rmp, Vector{Float64}(), Vector{Float64}(), 1.0, 1.0)
-end
-
-# linking constraints
-for i in 1:m
-    MPB.addconstr!(rmp, Vector{Float64}(), Vector{Float64}(), b[i], b[i])
-end
-
-# Artificial variables
-for i in 1:m
-    MPB.addvar!(rmp, [R+i], [1.0], 0.0, Inf, 10^4)  # slack
-    MPB.addvar!(rmp, [R+i], [-1.0], 0.0, Inf, 10^4)  # surplus
-end
-
-# Instanciate RMP
-mp = Linda.LindaMaster(rmp, R, m, b)
-
-# Generate some columns
-for r in 1:R
-    col = Linda.Column(1.0, ones(m), true, false, r, 0)
-    Linda.add_column!(mp, col)
-    # Add column again, this should not do anything
-    Linda.add_columns!(mp, [col])
-end
-
-@test mp.num_columns_rmp == R
-@test MPB.numvar(mp.rmp) == 2*m + R
+@test mp.num_columns_rmp == 2*R
+@test MPB.numvar(mp.rmp) == 2*m + 2*R
 
 
 # Solve RMP to optimality
 Linda.solve_rmp!(mp)
+s = MPB.getreducedcosts(mp.rmp)
+# check computation of reduced costs
+for i in 1:mp.num_columns_rmp
+    @test s[2*m+i] ≈ Linda.get_reduced_cost(mp.active_columns[i], mp.π, mp.σ)
+end
 
 @test mp.rmp_status == Linda.Optimal
 @test mp.mp_status == Linda.PrimalFeasible
-@test MPB.getobjval(mp.rmp) ≈ 1.0
+@test MPB.getobjval(mp.rmp) ≈ 0.0
 
-# Infeasible RMP
-# (de-activate artificial variables and )
-MPB.setvarUB!(rmp, vcat(zeros(2*m), Inf*ones(R)))  # de-activate artificial vars
+# Make RMP infeasible
+# de-activate artificial variables
+MPB.setvarUB!(rmp, vcat(zeros(2*m), Inf*ones(mp.num_columns_rmp)))
+# Change right-hand side
 MPB.setconstrLB!(rmp, vcat(ones(R), -ones(m)))
 MPB.setconstrUB!(rmp, vcat(ones(R), -ones(m)))
 mp.rhs_constr_link = -ones(m)
 
 Linda.solve_rmp!(mp)
 @test mp.rmp_status == Linda.PrimalInfeasible  # RMP is primal infeasible
+=#
